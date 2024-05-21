47cb7b0fe2f54764ce0bf97e080fd34125e55543
Lessalc <lucianoclessa@gmail.com>
2024-05-21 18:51:17 -0300

    Ajuste testes e CI-CD

--- src/test/java/com/fiap58/pedidos/unitTest/controller/ClienteControllerTest.java
+++ src/test/java/com/fiap58/pedidos/unitTest/controller/ClienteControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -34,6 +35,7 @@ import com.fiap58.pedidos.presenters.dto.saida.DadosClienteDto;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class ClienteControllerTest {
 
     @Autowired
